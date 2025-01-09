ddac11a18f32e6d2ed4784ccf7f5e62312621e18
Fabricio3237goncalves <fabriciogoncalvesoliveira3@gmail.com>
2025-01-09 10:26:11 -0300

    finalizado
    git commit -m  finalizado

---
+++ Documents/FS22-DiscoC/JavaScript-VariaveisEconstantes/index.js
@@ -0,0 +1,50 @@
+function mostrarValorDoInput 
+
+function GerarNome(){
+    if(dispatchEvent.value == ""){
+        alert("Informe o dia");
+        return;
+    }
+    let nome = "";
+    if(dispatchEvent.value >= 1 && dispatchEvent.value <= 8){
+        nome = "Desenvolvedor"
+        console.log("Nome", nome);
+    }else if( dispatchEvent.value >=9 && dia.value <=18)
+        nome = "Programador"
+        console.log ("Nome: "nome)   
+     }else if( dispatchEvent.value >=19 && dia.value <=26)
+        nome = "Estagiario"
+        console.log ("Nome: "nome) 
+    }else if( dispatchEvent.value >=27 && dia.value <=31)
+        nome = "Senior"
+        console.log ("Nome: "nome)    
+}
+
+switch(MessageChannel.value){
+    case "1":
+        nome += 'bugado;'; breack;
+    case "2":
+        nome += ' do CTRL C, CTRL V;'; breack;
+    case "3":
+            nome += 'Louco;'; breack;
+    case "4":
+        nome += ' que culpa o cache;'; breack;
+    case "5":
+        nome += 'das gambiarras;'; breack;
+    case "6":
+        nome += ' do windonws pirata;'; breack;
+    case "7":
+            nome += 'do commit bugado;'; breack;
+    case "8":
+        nome += ' faz nada;'; breack;  
+    case "9":
+        nome += 'fera;'; breack;
+    case "10":
+        nome += ' cego;'; breack;
+    case "11":
+            nome += 'nervoso;'; breack;
+    case "12":
+            nome += ' calmo;'; breack; 
+       
+
+}
